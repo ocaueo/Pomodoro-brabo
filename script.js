7e10f7af6d6052d1cb3666f8b4cecddd109b3742
ocaueo <caue4824@gmail.com>
2023-07-18 13:05:26 -0300

    falta um restinho pra ficar brabo

--- script.js
+++ script.js
@@ -21,6 +21,47 @@ function removeActive(){
     context.arc(100, 75, 50, angulo  * Math.PI);
     context.fillStyle = #FF0000;
     context.stroke();
-} */
+} 
 
-let inicio = 0
+let inicio = 0 */
+
+let config = document.querySelector('.configuracoes');
+settings.onclick = function(){
+    config.classList.add('active');
+}
+
+fecharSettings.onclick = function(){
+    config.classList.remove('active');
+}
+
+let tempo1 =number(localStorage.getItem('tempo1')) || 0;
+let tempo2 =number(localStorage.getItem('tempo2')) || 0;
+let tempo3 =number(localStorage.getItem('tempo3')) || 0;
+
+confirmar.onclick = function(){
+    tempo1 = pomodoroTime.value;
+    tempo2 = shortTime.value;
+    tempo3 = longTime.value;
+    
+    localStorage.setItem('tempo1', tempo1);
+    localStorage.setItem('tempo2', tempo2);
+    localStorage.setItem('tempo3', tempo3);
+    
+};
+
+state.onclick = function(){
+    
+}
+*/
+function contaSegundos(){
+    let segundos =0;
+    setInterval(function(){
+        if(segundos <59){
+            segundos = segundos + 1;
+            hora.innerHTML = segundos;
+            
+        }else
+    }
+}
+
+/*
